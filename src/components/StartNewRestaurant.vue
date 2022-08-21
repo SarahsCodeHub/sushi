b355a0ffb28f8da3558afbef9855c48475727c82
Sarah Michaelsen <sarah.michaelsen@sab.sachsen.de>
2022-08-21 17:00:50 +0200

    tests for start screen

--- src/components/StartNewRestaurant.vue
+++ src/components/StartNewRestaurant.vue
@@ -3,11 +3,11 @@
     <h1>Willkommen im Restaurant-Konfigurator</h1>
     <div>
       <label for="name">Wie soll Ihr Sushi-Restaurant heißen?</label>
-      <input v-model="name" type="text" name="name" id="name" placheholder="Sushi Bar F+P">
+      <input v-model="restaurantConfig.name" type="text" name="name" id="name" placheholder="Sushi Bar F+P" />
     </div>
     <div>
       <label for="amountOfSeats">Wie viele Sitzplätze hat Ihr Sushi-Restaurant?</label>
-      <input v-model="amountOfSeats" type="number" name="amountofSeats" id="amountofSeats">
+      <input v-model="restaurantConfig.amountOfSeats" type="number" name="amountOfSeats" id="amountOfSeats" />
     </div>
 
     <button @click="submit">Los geht's!</button>
--- test/basic-startNewRestaurant.test.ts
+++ test/basic-startNewRestaurant.test.ts
@@ -3,14 +3,27 @@ import { describe, expect, test } from 'vitest'
 import StartNewRestaurant from '../src/components/StartNewRestaurant.vue'
 
 describe('StartNewRestaurant', () => {
-  test('StartNewRestaurant default values set and useful', async () => {
+  test('StartNewRestaurant default values set and useful', () => {
     expect(StartNewRestaurant).toBeTruthy()
 
     const wrapper = mount(StartNewRestaurant)
-    const inputName = wrapper.find('input#name')
-    console.log(inputName)
-/*     expect(inputName.element.value).toBe('Sushi Bar F+P')
-    const inputAmountOfSeats = wrapper.find('input#amountOfSeats')
-    expect(inputAmountOfSeats.element.value).toBe(10) */
+    const inputName = wrapper.find('#name')
+    const inputAmountOfSeats = wrapper.find('#amountOfSeats')
+
+    expect(inputName.element.value).toBe('Sushi Bar F+P')
+    expect(inputAmountOfSeats.element.value).toBe('10')
+  })
+
+  test('StartNewRestaurant values change correct on input', () => {
+
+    const wrapper = mount(StartNewRestaurant)
+    const inputName = wrapper.find('#name')
+    const inputAmountOfSeats = wrapper.find('#amountOfSeats')
+
+    inputName.setValue('Sushi Bar Vitest')
+    inputAmountOfSeats.setValue('30')
+
+    expect(inputName.element.value).toBe('Sushi Bar Vitest')
+    expect(inputAmountOfSeats.element.value).toBe('30')
   })
 })
